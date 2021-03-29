6b520668aa1c2fc66efc8d62b2971361d8ab7d97
mOstanina <maryiaost@gmail.com>
2021-03-30 00:35:55 +0300

    mini13

--- DZ/MINI/JS/slick-cards.js
+++ DZ/MINI/JS/slick-cards.js
@@ -36,23 +36,55 @@ $('.slider-ditals').slick({
 
 
 $('.slider-photo-car').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
+  infinite: false,
   slidesToShow: 3,
-  slidesToScroll: 2,
+  slidesToScroll: 3, 
+  dots: true,
   responsive: [
     {
       breakpoint: 1280,
       settings: {
-        slidesToShow: 4,
+        slidesToShow: 2,
         slidesToScroll: 1,
-        infinite: true,
+        
         dots: true
       }
     },
     {
-      breakpoint: 850,
+      breakpoint: 890,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+      }
+    }
+  ]
+});
+
+$('.slider-photo-car-JCW').slick({
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 1, 
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 890,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
@@ -60,13 +92,12 @@ $('.slider-photo-car').slick({
       }
     },
     {
-      breakpoint: 650,
+      breakpoint: 750,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true
       }
     }
-
   ]
 });
