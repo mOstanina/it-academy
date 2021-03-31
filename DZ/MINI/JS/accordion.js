586eeef8218b4281210141e98761330292cf1301
mOstanina <maryiaost@gmail.com>
2021-03-31 14:00:28 +0300

    mini14

--- DZ/MINI/JS/accordion.js
+++ DZ/MINI/JS/accordion.js
@@ -1,10 +1,16 @@
+
+
+ $(document).ready(function ($){
 $(".fa-plus-circle").click(function(){
     $(".accordion-body").css("display", "block");
     $(this).css("display", "none");
     $(".fa-minus-circle").css("display", "block");
 })
+})
+$(document).ready(function ($){
 $(".fa-minus-circle").click(function(){
     $(".accordion-body").css("display", "none");
     $(this).css("display", "none");
     $(".fa-plus-circle").css("display", "block");
+})
 })
