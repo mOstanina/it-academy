df71958768a881be63a7db632c55d46a11f9957d
mOstanina <maryiaost@gmail.com>
2021-04-12 20:52:26 +0300

    dz-1-a2-2

--- FD2/DZ1/A2/a2.js
+++ FD2/DZ1/A2/a2.js
@@ -2,24 +2,47 @@ var userString = prompt("Введите строку содержащую нес
 var resultString;
 var stringToUser;
 
-function leftString(userString) {
-    //ищу пробелы пока только с левой стороны
-    var i = 0;
-    while (i < userString.length && userString[i] == "\x20") {
-        i++;
-    };
-    return userString.substring(i, userString.length);
-}
-function rightString(userString) {
-    //ищу пробелы только с левой стороны
-    var j = userString.length-1;
-    while (j > 0 && userString[j] == "\x20") {
+// превоначальный вариант:
+// function leftString(userString) {
+//     //ищу пробелы пока только с левой стороны
+//     var i = 0;
+//     while (i < userString.length && userString[i] == "\x20") {
+//         i++;
+//     };
+//     return userString.substring(i, userString.length);
+// }
+// function rightString(userString) {
+//     //ищу пробелы только с правой стороны
+//     var j = userString.length-1;
+//     while (j > 0 && userString[j] == "\x20") {
+//         j--;
+//     };
+//     return userString.substring(0, j+1);
+// }
+// function askAString(userString) {
+//     return "\u2606"+ rightString(leftString(userString)) +"\u2606";
+// }
+// stringToUser = askAString(userString);
+// console.log(stringToUser);
+//---------------------------------------------------------------------------
+
+// переделала в одну функцию:
+function askAString(userString) {
+    var j = userString.length - 1;
+    while (j > 0 && userString[j] == " ") {
         j--;
     };
-    return userString.substring(0, j+1);
+    var rightSide;
+    rightSide = userString.substring(0, j + 1);
+
+    var i = 0;
+    while (i < rightSide.length && rightSide[i] == " ") {
+        i++;
+    };
+    var leftSide;
+    leftSide = rightSide.substring(i, rightSide.length);
+
+    return "\u2606" + leftSide + "\u2606";
 }
-function askAString(userString) {
-    return "\u2606"+ rightString(leftString(userString)) +"\u2606";
-}
-stringToUser = askAString(userString);
-console.log(stringToUser);
+
+console.log(askAString(userString));
