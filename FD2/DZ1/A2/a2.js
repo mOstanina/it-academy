76ad690fdfb3bb9d02fbbf2db47c36bef20d90a9
mOstanina <maryiaost@gmail.com>
2021-04-14 20:05:15 +0300

    dz-1-a2-5

--- FD2/DZ1/A2/a2.js
+++ FD2/DZ1/A2/a2.js
@@ -1,20 +1,20 @@
 var enterString = prompt("Введите строку содержащую несколько слов");
 
-
 function askAString(userString) {
     var j = userString.length - 1;
     while (j > 0 && userString[j] == " ") {
         j--;
     };
- 
     var i = 0;
     while (i < userString.length && userString[i] == " ") {
         i++;
     };
     var stringToUser;
-    stringToUser = userString.substring(i, j+1);
-
+    if (i == userString.length) {
+        stringToUser = userString.substring(i, userString.length);
+    } else {
+        stringToUser = userString.substring(i, j + 1);
+    }
     return stringToUser;
 }
-
-console.log("\u2606" + askAString(enterString)+ "\u2606");
+console.log("\u2606" + askAString(enterString) + "\u2606");
