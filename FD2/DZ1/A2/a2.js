353372b086f5b4e3d7e08965490beacedaf4225f
mOstanina <maryiaost@gmail.com>
2021-04-15 20:46:01 +0300

    dz1-a2-7

--- FD2/DZ1/A2/a2.js
+++ FD2/DZ1/A2/a2.js
@@ -1,19 +1,26 @@
 var enterString = prompt("Введите строку содержащую несколько слов");
-
 function askAString(userString) {
-    var j = userString.length - 1;
-    while (j > 0 && userString[j] == " ") {
-        j--;
-    };
     var i = 0;
+    var j = userString.length - 1;
+    var stringToUser;
     while (i < userString.length && userString[i] == " ") {
         i++;
     };
-    var stringToUser;
     if (i == userString.length) {
         stringToUser = "";
-    } else {
-        stringToUser = userString.substring(i, j + 1);
+        console.log("строка состоит только из пробелов");
+    }
+    else {
+        while (j > 0 && userString[j] == " ") {
+            j--;
+        };
+        if ((j== userString.length-1) && (i == 0)) {
+            stringToUser = userString;
+            console.log("в строке нет пробелов");
+        } else {
+            stringToUser = userString.substring(i, j + 1);
+            console.log("в строке удалены начальные и конечные пробелы");
+        };
     }
     return stringToUser;
 }
