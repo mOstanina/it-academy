fb008aeae051f61a418771464ca7f2ad9807742c
mOstanina <maryiaost@gmail.com>
2021-04-16 18:43:49 +0300

    dz1-a3-1

---
+++ FD2/DZ1/A3/a3.js
@@ -0,0 +1,14 @@
+var enterString = prompt("Введите строку").toLowerCase();
+function askAString(userString) {
+    var newReplaceUserString = userString.replace(/[ ъъ.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/ё/g, "e");
+    var halfOfUserString = (Math.floor(newReplaceUserString.length / 2));
+    var lastIndex = newReplaceUserString.length - 1;
+    for (var i = 0; i < halfOfUserString; i++) {
+        if (newReplaceUserString[i] !== newReplaceUserString[lastIndex  - i]) {
+           return "это не палиндром";
+        } else {
+        return "это палиндром"
+        }
+    }
+}
+console.log(askAString(enterString));
