6c580efcc029df5685baefa2c853cdbac9975256
mOstanina <maryiaost@gmail.com>
2021-04-18 23:47:49 +0300

    dz2-1

--- FD2/DZ2/VOWELS/vowels.js
+++ FD2/DZ2/VOWELS/vowels.js
@@ -1,9 +1,12 @@
-userMessage = prompt("Веедите любую строку")
-
-function userString(userString) {
-    var newReplaceUserString = userString.toLowerCase();
-
-
-
+var userMessage = prompt("Веедите любую строку")
+function countOf(userString) {
+    var newUserString = userString.toLowerCase().split('');
+    var counter = 0;
+    for (var el of newUserString) {
+        if (el == "а" || el == "е" || el == "ё" || el == "и" || el == "о" || el == "у" || el == "ы" || el == "э" || el == "ю" || el == "я") {
+            counter++;
+        }
+    }
+    return counter;
 };
-console.log(userString(userMessage));
+console.log(countOf(userMessage));
