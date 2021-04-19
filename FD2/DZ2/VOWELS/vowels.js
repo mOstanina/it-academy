a1116d5efcd08492317809f102a556cd5f38a584
mOstanina <maryiaost@gmail.com>
2021-04-19 22:43:57 +0300

    dz2-vowels

--- FD2/DZ2/VOWELS/vowels.js
+++ FD2/DZ2/VOWELS/vowels.js
@@ -1,12 +1,13 @@
 var userMessage = prompt("Веедите любую строку")
 function countOf(userString) {
     var newUserString = userString.toLowerCase().split('');
-    var counter = 0;
-    for (var el of newUserString) {
-        if (el == "а" || el == "е" || el == "ё" || el == "и" || el == "о" || el == "у" || el == "ы" || el == "э" || el == "ю" || el == "я") {
-            counter++;
-        }
-    }
-    return counter;
+    var vowels = "аеёиоуыэюя";
+    var res = newUserString.reduce(function (count, val) {
+        if (vowels.indexOf(val) !== -1) {
+            count++;
+        };
+        return count;
+    },0)
+    return res;
 };
 console.log(countOf(userMessage));
