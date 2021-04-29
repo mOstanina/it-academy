f38feebdafb1b3e48d50b300704c2bf17b28cc8f
mOstanina <maryiaost@gmail.com>
2021-04-29 21:04:09 +0300

    b6-4

--- FD2/DZ3/B6/b6.js
+++ FD2/DZ3/B6/b6.js
@@ -1,41 +1,35 @@
-
-var wrapP = buildWrapper();// по заданию как аргумент принимается сразу "P"
-// в консоли выдает "Uncaught TypeError: wrapP is not a function at b6.js:19",
-//ну да, это не функция, а ссылка на фукцию buildWrapper
-function buildWrapper(userString) {
- 
-    var newUserString = userString.split(" "); //разбиваю строку по пробелу на массив слов 
-    console.log(newUserString); //проверяю что вышло, потом удалю
-    var languageAtr = {
-        'lang:"ru"': "lang='ru'",
-        "lang:'ru'": "lang='ru'",
-        'lang:"en"': "lang='en'",
-        "lang:'en'": "lang='en'",
+var wrapP = buildWrapper("P");
+var wrapH1 = buildWrapper("H1");
+function buildWrapper(tagNew) {
+    var a = function getUserString(userStr) {
+        var arrayOfUserString = userStr.split(" ");
+        var languageArr = ['{lang:"ru"}', "{lang:'ru'}", '{lang:"en"}', "{lang:'en'}",]
+        for (var i = 0; i < arrayOfUserString.length; i++) {
+            var c = arrayOfUserString[i];
+            if (languageArr.indexOf(c) !== -1) {
+                var indexInArray = arrayOfUserString.indexOf(c);
+                var langOfuserString = " " + arrayOfUserString.splice(indexInArray, arrayOfUserString.length).join(" ").replace(/[:]/g, "=").replace(/[{}]/g, "");
+            }else{
+                langOfuserString ="";
+            }
+        }
+        arrayOfUserString = arrayOfUserString.join(" ");
+        function convertString(stringToConvert) {
+            var mnemo = {
+                "<": "&lt",
+                ">": "&gt",
+                "&": "&amp",
+                "'": "&#039",
+                '"': "&quot",
+            };
+            function changeSymbol(symbol) {
+                return mnemo[symbol];
+            }
+            return stringToConvert.replace(/[<&>\'\"]/g, changeSymbol);
+        }
+        return ("\<" + tagNew  + langOfuserString + "\>" + convertString(arrayOfUserString) + "\<\/" + tagNew + "\>");
     };
-
-    //тут пытаюсь проверить полученный массив на наличие в нем ключа объекта но выдает -1, тк не находит 
-    if (newUserString.indexOf('lang:"ru"')) {
-        console.log(newUserString.indexOf(Object.keys(languageAtr)))
-    };
-   
-}
-console.log(wrapP("Однажды в студёную зимнюю пору", { lang: "ru" }));
-
-
-
-//функция меняющая символы на мнемонику:
-//тут все работает
-function convertString(stringToConvert) {
-    var mnemo = {
-        "<": "&lt",
-        ">": "&gt",
-        "&": "&amp",
-        "'": "&#039",
-        '"': "&quot",
-    };
-    function changeSymbol(symbol) {
-        return mnemo[symbol];
-    }
-    return stringToConvert.replace(/[<&>\'\"]/g, changeSymbol);
-}
-console.log(convertString("привет &привет>  < привет gbhg"));
+    return a;
+};
+console.log(wrapP("Однажды в студёную зимнюю & пору, {lang:'ru'}"));
+// console.log(wrapH1("СТИХИ", { align: "center", title: "M&M's" }));
