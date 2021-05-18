c70c669824e3f9f55885039f3224a8477c60de53
mOstanina <maryiaost@gmail.com>
2021-05-18 12:49:29 +0300

    DZ5-1

--- FD2/DZ5/DYN_FORM/DYN_FORM.js
+++ FD2/DZ5/DYN_FORM/DYN_FORM.js
@@ -15,7 +15,6 @@ var formDef2 = [{ label: 'Фамилия:', kind: 'longtext', name: 'lastname' }
 function dunForm(tag, content) {
     var str = "";
     var newForm = document.createElement(tag);
-    console.log(newForm);
     newForm.setAttribute("action", "https://fe.it-academy.by/TestForm.php");
     newForm.setAttribute("id", "myForm");
     var contElem = document.getElementById("foForm");
@@ -24,39 +23,73 @@ function dunForm(tag, content) {
     function createTag(tagName, con) {
         var newTaginForm = document.createElement(tagName);//создаю новый тег внутри формы
         //   console.log(newTaginForm);
-        if (tagName === "input") {
-            newTaginForm.setAttribute("type", content[i].kind);
-            newTaginForm.setAttribute("name", content[i].name);
-        }
-        if (content[i].kind === "check") {
-            newTaginForm.setAttribute("type", "checkbox");
-            newTaginForm.checked = true;
-        }
-        var newTextInForm = document.createTextNode(con);
+        var newTextInForm = document.createTextNode(con);// текст
         var innerElem = document.getElementById("myForm");//тег куда вставляю строчку
-        newTaginForm.appendChild(newTextInForm);
-        innerElem.appendChild(newTaginForm);
+        newTaginForm.appendChild(newTextInForm);//привязываю текст к элементу
+        innerElem.appendChild(newTaginForm);//привязываю элемент к дереву
         newTaginForm.innerHTML = con;
-
+        if (tagName !== "label" && tagName !== "br"&& tagName !== "span"&& tagName !== "hr") {
+            if (content[i].kind === "submit") {
+                newTaginForm.setAttribute("type", "submit");
+            }
+            else if (content[i].kind === "check") {
+                newTaginForm.setAttribute("type", "checkbox");
+                newTaginForm.checked = true;
+            }
+            else if (tagName === "select" && content[i].kind === "combo") {
+                newTaginForm.setAttribute("id", "sel");
+                newTaginForm.setAttribute("name", content[i].name);
+            }
+            else if (content[i].kind === "radio") {
+                newTaginForm.setAttribute("type", "radio");
+                newTaginForm.setAttribute("name", content[i].name);
+                newTaginForm.setAttribute("value", content[i].variants[j].value);
+            } else {
+                newTaginForm.setAttribute("type", "text");
+                newTaginForm.setAttribute("name", content[i].name);
+                if (content[i].kind === "longtext" || content[i].kind === "memo") {
+                    newTaginForm.style.width = "800px";
+                }
+                if (content[i].kind === "shorttext") {
+                    newTaginForm.style.width = "200px";
+                }
+                if (content[i].kind === "number") {
+                    newTaginForm.style.width = "50px";
+                }
+            }
+        }
     }
-
+    function createOption() {
+        var opt = document.createElement("option");//создаю новый тег
+        var sele = document.getElementById("sel");//тег куда вставляю строчку
+        var textInOpt = document.createTextNode(content[i].variants[l].text);// текст
+        opt.appendChild(textInOpt);
+        sele.appendChild(opt);
+        opt.setAttribute("value", content[i].variants[l].value);
+    }
     ////////////////////////////////////////////////////////////////////////////////
     for (var i = 0; i < content.length; i++) {
-        //   console.log(content[i].label);
-        console.log(content[i].kind);
         str += createTag("label", content[i].label);
-        if (content[i].kind === "radio") {
-            for (var j = 0; j < content[i].variants.length; j++) {
-                console.log(content[i].variants[j].text);
+        if (content[i].kind === "memo") {
+            str += createTag("br", "") + createTag("textarea", "")
+        } else if (content[i].kind === "radio") {
+            var j;
+            for (j = 0; j < content[i].variants.length; j++) {
                 str += createTag("input") + createTag("span", content[i].variants[j].text)
-                newTaginForm.setAttribute("value", "!!!!");
             }
-            str +=createTag("br") 
-        }else{
-            str += createTag("input", content[i].kind) + createTag("br") 
+        } else if (content[i].kind === "combo") {
+            str += createTag("select", "")
+            var l;
+            for (l = 0; l < content[i].variants.length; l++) {
+                str += createOption("option", content[i].variants[l].text)
+            }
         }
-       
+        else {
+            str += createTag("input", content[i].kind)
+        }
+        str += createTag("br", "")
     }
+    str += createTag("hr", "")
 }
-//dunForm("form", formDef2);
 dunForm("form", formDef1);
+dunForm("form", formDef2);
