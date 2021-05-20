4bc0109338727e429ced5068e379ca6eba245d33
mOstanina <maryiaost@gmail.com>
2021-05-20 23:45:34 +0300

    b4-2

--- FD2/DZ5/B4/b4.js
+++ FD2/DZ5/B4/b4.js
@@ -1,60 +1,56 @@
 "use strict"
 function metagramm(word1, word2) {
-
     var vocabulary = ["ЛУЖА", "МУЗА", "ЛИРА", "МЕХА", "ЛИГА", "ТАРА", "ЛИПА", "ТУРА", "ПАРК", "ЛОЖЬ", "ЛУПА", "ПЛОТ", "МУРА", "ПАУК", "ПАУТ", "ПЛУТ", "ЛОЖА", "СЛОТ", "ПАРА"];
-    var result = [];
-    var set1 = new Set();
-    var set2 = new Set();
-    var set3 = new Set();
-    var set4 = new Set();
-    //while (word1 !== word2) {
-    for (var i = 0; i < vocabulary.length; i++) {
-        set1.add(vocabulary[i].slice(0, 1));
-        set2.add(vocabulary[i].slice(1, 2));
-        set3.add(vocabulary[i].slice(2, 3));
-        set4.add(vocabulary[i].slice(3));
-    };
-    set1 = Array.from(set1);
-    set2 = Array.from(set2);
-    set3 = Array.from(set3);
-    set4 = Array.from(set4);
-    var arrayOfSets = [];
-    arrayOfSets.push(set1);
-    arrayOfSets.push(set2);
-    arrayOfSets.push(set3);
-    arrayOfSets.push(set4);
-    console.log(arrayOfSets);
-    console.log(set1)// ["Л", "М", "Т", "П", "С"]
-    console.log(set2)// ["У", "И", "Е", "А", "О", "Л"]
-    console.log(set3)// ["Ж", "З", "Р", "Х", "Г", "П", "О", "У"]
-    console.log(set4)// ["А", "К", "Ь", "Т"]
-
-    for (var i = 0; i < word1.length; i++) {
-
-        if (word1.slice(0 + i, 1 + i) !== word2.slice(0 + i, 1 + i)) {
-            var newLetter = word1.slice(0 + i, 1 + i);// заменяемя буква 
-            console.log(newLetter);
-
-            for (var j = 0; j < arrayOfSets.length; j++) {
-                console.log(set1[j]);//буква для замены
-                if (newLetter !== arrayOfSets[j]) {
-                    var newWord = word1.replace(arrayOfSets[set1[j]], newLetter);
-                    console.log(newWord);
-                    for (var n = 0; n < vocabulary.length; n++) {
-                        if (newWord === vocabulary[n]) {
-                            result.push(newWord);
-                            metagramm(newWord, word2)
-                        }
-                    }
-                }
-
+    var result;
+    // фунция сравнения двух слов
+    function comparison(value1, value2) {
+        var count = 0;
+        for (var i = 0; i < value1.length; i++) {
+            if (value1[i] === value2[i]) {
+                count++;
             }
-
         }
+        if (count === 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    var exception = [];// создаю массив для исключений, т.е для слов котрые подходят
+    function JJJ(word1) {
+        for (var j = 0; j < vocabulary.length; j++) {//перебираю весь словарь
+            var m = vocabulary[j];
+            if (comparison(word1, vocabulary[j]) && (!exception.includes(vocabulary[j]))) {
+                m = vocabulary[j];
+                exception.push(vocabulary[j]);
+                JJJ(vocabulary[j])
+            }
+        }
+    }
+    JJJ(word1);
+    //проверяю чтобы последнее слово отличалось от конечного только 1 символом
+    var len = exception.length - 1;
+    if (!comparison((exception[len]), word2)) {
+        exception.pop(exception[len])
 
-    };
-
+    }
+    // из полученного массива исключений удаляю те, что отличаются друг от друга только 1 буквой
+    //кроме последнего (для связи с последующей цепочкой)
+    var metagrammArray = [];
+    for (var l = 0; l < exception.length; l++) {
+        if (comparison(word1, exception[l]) && (!metagrammArray.includes(exception[l]))) {
+            metagrammArray.push(exception[l]);
+        }
+    }
+    //узнаю индексы этих слов в массиве исключений и удаляю
+    for (var n = 0; n < metagrammArray.length - 1; n++) {
+        var ind;
+        ind = exception.indexOf(metagrammArray[n])
+        delete exception[ind]
+    }
+    result = word1 + " " + exception.join(" ") + " " + word2;
     console.log(result);
-    //  }
+    return result
 }
 metagramm("ЛИСА", "ЛОСЬ");
+metagramm("МУХА", "СЛОН");
