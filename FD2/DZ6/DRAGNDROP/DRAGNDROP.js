ee2f1acd6416a5e477e21a3e1dceeae45bf0033c
mOstanina <maryiaost@gmail.com>
2021-05-24 21:05:38 +0300

    DRAGNDROP-2

--- FD2/DZ6/DRAGNDROP/DRAGNDROP.js
+++ FD2/DZ6/DRAGNDROP/DRAGNDROP.js
@@ -1,57 +1,55 @@
 "use strict";
 window.addEventListener('load', changeImgPosition, false);
+//document.addEventListener('mousemove', moveAt, false);
 function changeImgPosition() {
+    var img = null;
+    var globalZIndex = 10;
     var elems = document.body.getElementsByTagName("img");
+    var otsup = 0;
     for (var i = 0; i < elems.length; i++) {
-        var elemsWidth = elems[i].offsetWidth;
-        var yOffset = (elemsWidth + 10) * i; //добавляю отступы 10px чтобы не слипались картинки
-        elems[i].style = "position:absolute; left: " + yOffset + "px";
         elems[i].setAttribute("id", "img" + i);
         elems[i].setAttribute("z-index", 1);
-        elems[i].setAttribute("ondragstart", "imgDragStart(event)"); 
-    }
-}
-var img = null;
-var globalZIndex = 10;
-function imgDragStart(EO) {    // началось перетаскивание
-    EO = EO || window.event;
-    EO.preventDefault();
-   // console.log('starting drag id=' + EO.target.id);
-    img = EO.target;
-    moveEl(img);
-    function moveEl(img) {
-        img.onmousedown = function (e) {
-            var coords = getCoords(img);
-            var shiftX = e.pageX - coords.left;
-            var shiftY = e.pageY - coords.top;
-            moveAt(e);
+        var left = getCoords(elems[i]).left + otsup;
+        var top = getCoords(elems[i]).top;
+        otsup += elems[i].offsetWidth
+        elems[i].style = "position:absolute; left:" + left + "px; top:" + top + "px;";
+        var img = elems[i];
+        moveEl(img);
 
-            globalZIndex++; // над другими элементами
-            img.style.zIndex = globalZIndex;
-
-            function moveAt(e) {
-                img.style.left = e.pageX - shiftX + 'px';
-                img.style.top = e.pageY - shiftY + 'px';
-            }
-
-            document.onmousemove = function (e) {
+        function moveEl(img) {
+            img.onmousedown = function (e) {
+                var coords = getCoords(img);
+                var shiftX = e.pageX - coords.left;
+                var shiftY = e.pageY - coords.top;
                 moveAt(e);
-            };
-            
-            img.onmouseup = function () {
-                document.onmousemove = null;
-                img.onmouseup = null;
+
+                globalZIndex++; // над другими элементами
+                img.style.zIndex = globalZIndex;
+
+                function moveAt(e) {
+                    img.style.left = e.pageX - shiftX + 'px';
+                    img.style.top = e.pageY - shiftY + 'px';
+                }
+
+                document.onmousemove = function (e) {
+                    moveAt(e);
+                };
+
+                img.onmouseup = function () {
+                    document.onmousemove = null;
+                    img.onmouseup = null;
+                };
+            }
+            img.ondragstart = function () {
+                return false;
             };
         }
-        img.ondragstart = function () {
-            return false;
-        };
     }
-    function getCoords(elem) {   // кроме IE8-
-        var box = elem.getBoundingClientRect();
-        return {
-            top: box.top + pageYOffset,
-            left: box.left + pageXOffset
-        };
-    }
+}
+function getCoords(elem) {   // кроме IE8
+    var box = elem.getBoundingClientRect();
+    return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+    };
 }
---
+++ FD2/DZ6/VALID_FORM/VALID_FORM.js
@@ -0,0 +1,103 @@
+"use strict";
+function createErrorMessage(name) {
+    var span = document.getElementsById(name);//тег куда вставляю строчку
+    console.log(span)
+    var textInOpt = document.createTextNode("!!!!!!!");// текст
+    span.appendChild(textInOpt);
+    console.log("!!!!")
+}
+
+var formTag=document.forms.INFO; // а можно было найти через getElementById
+formTag.addEventListener('submit',validateInfoForm,false); // назначаем обработчик события submit
+
+function validateInfoForm(EO) {
+    EO=EO||window.event;
+   // try {
+        var formTag=document.forms.INFO;
+
+        var fioField=formTag.elements.FIO; // а можно было найти через getElementById
+        var fioValue=fioField.value; // текстовое значение
+
+        var ageField=formTag.elements.AGE;
+        var ageValue=parseInt(ageField.value.trim()); // текст -> число
+
+        var agreeField=formTag.elements.AGREE;
+        var agreeValue=agreeField.checked; // логическое значение
+
+        var designField=formTag.elements.DESIGN;
+        var designValue=designField.value; // строковое значение
+
+        // каждая радиокнопка является отдельным элементом формы
+        // форма.elements[имя] для радиогруппы вернёт КОЛЛЕКЦИЮ
+        // т.е. несколько элементов формы с одинаковым именем
+        // и коллекция.value вернёт value ВЫБРАННОЙ радиокнопки
+        // (могут быть проблемы совместимости с IE, проверяйте!)
+        var langField=formTag.elements.LANG;
+        var langValue=langField.value; // строковое значение
+
+        if ( fioValue.length>30 ) {
+            alert('Введите пожалуйста ФИО не длиннее 30 символов!');
+            fioField.focus(); // фокусируем элемент и прокручиваем к нему
+            EO.preventDefault(); // форма не будет отправлена на сервер
+            return;
+        }
+
+        if ( isNaN(ageValue) ) {
+            alert('Введите пожалуйста в поле возраста корректную цифру!');
+            //ageField.focus();
+            createErrorMessage("AGE") 
+            EO.preventDefault(); // форма не будет отправлена на сервер
+            return;
+        }
+
+        if ( ageValue<16 ) {
+            alert('Возраст должен быть не менее 16 лет!');
+            ageField.focus();
+            EO.preventDefault(); // форма не будет отправлена на сервер
+            return;
+        }
+
+        if ( ageValue==222 ) {
+            alert('Возраст не может быть таким!');
+            ageField.focuspokus(); // случайная ошибка!
+            EO.preventDefault();
+            return;
+        }
+
+        if ( !agreeValue ) {
+            alert('Вы не согласились с правилами сайта!');
+            agreeField.focus();
+            EO.preventDefault(); // форма не будет отправлена на сервер
+            return;
+        }
+
+        if ( designValue==2 ) {
+            alert('Вы выбрали вычурный дизайн, подумайте ещё!');
+            designField.focus();
+            EO.preventDefault(); // форма не будет отправлена на сервер
+            return;
+        }
+
+        if ( langValue=="" ) { // если ничего не выбрано - radio показывает пустую строку
+            alert('Вы не выбрали язык!');
+            // langField.focus(); сфокусировать ВСЮ радиокнопку не получится
+            // но можно промотать страницу к первой из радиокнопок:
+            document.getElementById('LANG11').scrollIntoView();
+            EO.preventDefault(); // форма не будет отправлена на сервер
+            return;
+        }
+        if ( langValue==33 ) {
+            alert('Вы выбрали немецкий язык, подумайте ещё!');
+            //langField.focus(); сфокусировать ВСЮ радиокнопку не получится
+            document.getElementById('LANG11').scrollIntoView();
+            EO.preventDefault(); // форма не будет отправлена на сервер
+            return;
+        }
+
+        // валидация успешная - форма будет отправлена на сервер
+    //}
+    // catch ( ex ) {
+    //     alert('Извините, что-то пошло не так, неожиданный сбой! Пересмотрите заполнение формы, возможно, это всё из-за вас!');
+    //     EO.preventDefault(); // что-то пошло не так - считаем что валидация не прошла
+    // }
+}
