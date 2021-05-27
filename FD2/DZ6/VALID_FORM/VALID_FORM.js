d8cad1c598e654fe48e2047bfbc8d7d3b9f131e9
mOstanina <maryiaost@gmail.com>
2021-05-27 22:22:30 +0300

    validform-2

--- FD2/DZ6/VALID_FORM/VALID_FORM.js
+++ FD2/DZ6/VALID_FORM/VALID_FORM.js
@@ -1,225 +1,162 @@
 "use strict";
 var formTag = document.forms.INFO;
+var errors = [];
+
 var developersField = formTag.elements.DEVELOPERS;
-var developersValue = developersField.value;
 developersField.addEventListener("blur", createMessageErrorDEVELOPERS, false);
 function createMessageErrorDEVELOPERS(EO) {
     EO = EO || window.event;
-    console.log("!!!")
-    var textInSpan = document.createTextNode("");
+    var developersValue = developersField.value;
     var span = document.getElementById("DEVELOPERS");
-    span.appendChild(textInSpan);
-    console.log(developersValue.length)
-    var text;
     if (developersValue.length > 5 || developersValue === "") {
-        console.log("Error!")
-        text = "Error";
-        EO.preventDefault();
+        span.innerHTML = "Error";
+        errors[0] = "Error"
     } else {
-        console.log("notError!")
-        text = "";
+        span.innerHTML = "";
+        errors[0] = "notError"
     }
-    textInSpan.innerHTML = text;
-
     return;
 }
-
 //////////
 var websiteField = formTag.elements.WEBSITE;
-var websiteValue = websiteField.value;
 websiteField.addEventListener("blur", createMessageErrorWEBSITE, false);
 function createMessageErrorWEBSITE(EO) {
     EO = EO || window.event;
-    console.log("!!!")
-    var textInSpan = document.createTextNode("");
+    var websiteValue = websiteField.value;
     var span = document.getElementById("WEBSITE");
-    var text;
-    textInSpan.innerHTML = text;
     if (websiteValue === "") {
-        console.log("Error!")
-        text = "Error";
-        EO.preventDefault();
+        span.innerHTML = "Error";
+        errors[1] = "Error";
     } else {
-        console.log("notError!");
-        text = "";
+        span.innerHTML = "";
+        errors[1] = "notError"
     }
-    span.appendChild(textInSpan);
     return;
 }
 //////////
 var websiteUrlField = formTag.elements.WEBSITEURL;
-var websiteUrlValue = websiteUrlField.value;
 websiteUrlField.addEventListener("blur", createMessageErrorWEBSITEURL, false);
 function createMessageErrorWEBSITEURL(EO) {
     EO = EO || window.event;
-    console.log("!!!")
-    var textInSpan = document.createTextNode("");
+    var websiteUrlValue = websiteUrlField.value;
     var span = document.getElementById("WEBSITEURL");
-    span.appendChild(textInSpan);
-    var text;
     if (websiteUrlValue === "") {
-        console.log("Error!")
-        text = "Error";
-        EO.preventDefault();
+        span.innerHTML = "Error";
+        errors[2] = "Error"
     } else {
-        console.log("notError!")
-        text = "";
+        span.innerHTML = "";
+        errors[2] = "notError"
     }
-    textInSpan.innerHTML = text;
     return;
 }
 //////////
-var datelField = formTag.elements.DATE;
-var datelValue = parseInt(datelField.value.trim());
-datelField.addEventListener("blur", createMessageErrorDATE, false);
+var dateField = formTag.elements.DATE;
+dateField.addEventListener("blur", createMessageErrorDATE, false);
 function createMessageErrorDATE(EO) {
     EO = EO || window.event;
-    console.log("!!!")
-    var textInSpan = document.createTextNode("");
-    var span = document.getElementById("WEBSITEURL");
-    span.appendChild(textInSpan);
-    var text;
-    if (isNaN(datelValue)) {
-        console.log("Error!")
-        text = "Error";
-        EO.preventDefault();
+    var dateValue = parseInt(dateField.value.trim());
+    var span = document.getElementById("DATE");
+    if (isNaN(dateValue)) {
+        span.innerHTML = "Error";
+        errors[3] = "Error"
     } else {
-        console.log("notError!")
-        text = "";
+        span.innerHTML = "";
+        errors[3] = "notError"
     }
-    textInSpan.innerHTML = text;
     return;
 }
 //////////
 var visitorsField = formTag.elements.VISITORS;
-var visitorsValue = parseInt(visitorsField.value.trim());
 visitorsField.addEventListener("blur", createMessageErrorVISITORS, false);
 function createMessageErrorVISITORS(EO) {
     EO = EO || window.event;
-    console.log("!!!")
-    var textInSpan = document.createTextNode("");
+    var visitorsValue = parseInt(visitorsField.value.trim());
     var span = document.getElementById("VISITORS");
-    span.appendChild(textInSpan);
-    var text;
     if (isNaN(visitorsValue)) {
-        console.log("Error!")
-        text = "Error";
-        EO.preventDefault();
+        span.innerHTML = "Error";
+        errors[4] = "Error"
     } else {
-        console.log("notError!")
-        text = "";
+        span.innerHTML = "";
+        errors[4] = "notError"
     }
-    textInSpan.innerHTML = text;
     return;
 }
 //////////
 var emailField = formTag.elements.EMAIL;
-var emailValue = emailField.value;
 emailField.addEventListener("blur", createMessageErrorEMAIL, false);
 function createMessageErrorEMAIL(EO) {
     EO = EO || window.event;
-    console.log("!!!")
-    var textInSpan = document.createTextNode("");
+    var emailValue = emailField.value;
     var span = document.getElementById("EMAIL");
-    span.appendChild(textInSpan);
-    var text;
     if (emailValue === "") {
-        console.log("Error!")
-        text = "Error";
-        EO.preventDefault();
+        span.innerHTML = "Error";
+        errors[5] = "Error"
     } else {
-        console.log("notError!")
-        text = "";
+        span.innerHTML = "";
+        errors[5] = "notError"
     }
-    textInSpan.innerHTML = text;
     return;
 }
 //////////
 var headingField = formTag.elements.HEADING;
-var headingValue = headingField.value;
 headingField.addEventListener("blur", createMessageErrorHEADING, false);
 function createMessageErrorHEADING(EO) {
     EO = EO || window.event;
-    console.log("!!!")
-    var textInSpan = document.createTextNode("");
+    var headingValue = headingField.value;
     var span = document.getElementById("HEADING");
-    span.appendChild(textInSpan);
-    var text;
-    if (headingValue === 3) {
-        console.log("Error!")
-        text = "Error";
-        EO.preventDefault();
+    if (headingValue == 3) {
+        span.innerHTML = "Error";
+        errors[6] = "Error"
     } else {
-        console.log("notError!")
-        text = "";
+        span.innerHTML = "";
+        errors[6] = "notError"
     }
-    textInSpan.innerHTML = text;
     return;
 }
-//////////
+// //////////
 var placementField = formTag.elements.PLACEMENT;
 var placementValue = placementField.value;
-placementField.addEventListener("blur", createMessageErrorPLACEMENT, false);
-function createMessageErrorPLACEMENT(EO) {
-    EO = EO || window.event;
-    console.log("!!!")
-    var textInSpan = document.createTextNode("");
+function createMessageErrorPLACEMENT() {
     var span = document.getElementById("PLACEMENT");
-    span.appendChild(textInSpan);
-    var text;
-    if (placementValue === 33) {
-        console.log("Error!")
-        text = "Error";
-        EO.preventDefault();
+    if (!document.getElementById('11').checked && !document.getElementById('22').checked && !document.getElementById('33').checked) {
+        span.innerHTML = "Error";
+        errors[7] = "Error"
     } else {
-        console.log("notError!")
-        text = "";
+        span.innerHTML = "";
+        errors[7] = "notError"
     }
-    textInSpan.innerHTML = text;
     return;
 }
 //////////
 var resolutionField = formTag.elements.RESOLUTION;
-var resolutionValue = resolutionField.checked;
 resolutionField.addEventListener("blur", createMessageErrorRESOLUTION, false);
 function createMessageErrorRESOLUTION(EO) {
     EO = EO || window.event;
-    console.log("!!!")
-    var textInSpan = document.createTextNode("");
+    var resolutionValue = resolutionField.checked;
     var span = document.getElementById("RESOLUTION");
-    span.appendChild(textInSpan);
-    var text;
     if (!resolutionValue) {
-        console.log("Error!")
-        text = "Error";
-        EO.preventDefault();
+        span.innerHTML = "Error";
+        errors[8] = "Error"
     } else {
-        console.log("notError!")
-        text = "";
+        span.innerHTML = "";
+        errors[8] = "notError"
     }
-    textInSpan.innerHTML = text;
     return;
 }
 //////////
 var discriptionField = formTag.elements.DESCRIPTION;
-var discriptionValue = discriptionField.value;
 discriptionField.addEventListener("blur", createMessageErrorDESCRIPTION, false);
 function createMessageErrorDESCRIPTION(EO) {
     EO = EO || window.event;
-    console.log("!!!")
-    var textInSpan = document.createTextNode("");
+    var discriptionValue = discriptionField.value;
     var span = document.getElementById("DESCRIPTION");
-    span.appendChild(textInSpan);
-    var text;
     if (discriptionValue === "") {
-        console.log("Error!")
-        text = "Error";
-        EO.preventDefault();
+        span.innerHTML = "Error";
+        errors[9] = "Error"
     } else {
-        console.log("notError!")
-        text = "";
+        span.innerHTML = "";
+        errors[9] = "notError"
     }
-    textInSpan.innerHTML = text;
     return;
 }
 //////////////////////////////////////////////////
@@ -234,9 +171,51 @@ function validateInfoForm(EO) {
         createMessageErrorVISITORS(EO);
         createMessageErrorEMAIL(EO);
         createMessageErrorHEADING(EO);
-        createMessageErrorPLACEMENT(EO);
+        createMessageErrorPLACEMENT();
         createMessageErrorRESOLUTION(EO);
         createMessageErrorDESCRIPTION(EO);
+        switch ("Error") {
+            case errors[0]:
+                developersField.focus();
+                EO.preventDefault();
+                break;
+            case errors[1]:
+                websiteField.focus();
+                EO.preventDefault();
+                break;
+            case errors[2]:
+                websiteUrlField.focus();
+                EO.preventDefault();
+                break;
+            case errors[3]:
+                dateField.focus();
+                EO.preventDefault();
+                break;
+            case errors[4]:
+                visitorsField.focus();
+                EO.preventDefault();
+                break;
+            case errors[5]:
+                emailField.focus();
+                EO.preventDefault();
+                break;
+            case errors[6]:
+                headingField.focus();
+                EO.preventDefault();
+                break;
+            case errors[7]:
+                placementValue.scrollIntoView();
+                EO.preventDefault();
+                break;
+            case errors[8]:
+                resolutionField.focus();
+                EO.preventDefault();
+                break;
+            case errors[9]:
+                discriptionField.focus();
+                EO.preventDefault();
+                break;
+        }
     }
     catch (ex) {
         alert('Извините, что-то пошло не так, неожиданный сбой! Пересмотрите заполнение формы, возможно, это всё из-за вас!');
