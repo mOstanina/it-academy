a7d88178a5e01e00e5c5f5b6df77a6442966c87b
mOstanina <maryiaost@gmail.com>
2021-06-09 22:46:00 +0300

    clock_dom-1

--- FD2/DZ6/B3/B3.js
+++ FD2/DZ6/B3/B3.js
@@ -1,3 +1,245 @@
+// // function calc(equation) {
+// //   // for (var i = 0; i < equation.length; i++) {
+// //   //     if (equation[i] === "(") {
+// //   //         start = equation.slice(i);//отрезаю все до скобки
+// //   //         console.log(start)
+// //   //     }
+// //   //     if (equation[i] === ")") {
+// //   //         end = start.slice(1, i - 2);//убираю скобку в начале и отрезаю скобку в конце и один знак
+// //   //         console.log(end)
+// //   //     }
+// //   // }
+// //   var arrayOfsymbols = equation.split("")//преобразую в массив
+
+// //   console.log(arrayOfsymbols);
+
+// //   for (var j = 0; j < arrayOfsymbols.length; j++) {
+// //     if (arrayOfsymbols[j] === "(" && (arrayOfsymbols[j + 1]) === "-") { //если после скобки идет минус нужно сделать отрицательное число
+// //       arrayOfsymbols[j + 1] + arrayOfsymbols[j + 2]
+// //       arrayOfsymbols = arrayOfsymbols.slice(0, j + 1).concat(arrayOfsymbols[j + 1] + arrayOfsymbols[j + 2], arrayOfsymbols.slice(j + 3))
+// //       console.log(arrayOfsymbols)
+// //     }
+// //   }
+
+// //   var startDot
+// //   var endtDot
+// //   var fractionalNumber
+// //   for (var k = 0; k < arrayOfsymbols.length; k++) { // где есть точка делаю дробное число
+// //     if (arrayOfsymbols[k] === ".") {
+// //       console.log(arrayOfsymbols[k]);
+// //       startDot = k - 1;
+// //       endtDot = k + 1
+// //     }
+// //     while (!isNaN(parseInt(arrayOfsymbols[startDot]))) {
+// //       startDot--
+// //       console.log(arrayOfsymbols[startDot]);
+// //     }
+// //     while (!isNaN(parseInt(arrayOfsymbols[endtDot])) && endtDot < arrayOfsymbols.length) {
+// //       console.log("!!!!");
+// //       endtDot++
+// //     }
+
+
+// //   }
+
+// //   console.log(endtDot);
+// //   fractionalNumber = arrayOfsymbols.slice(startDot + 1, endtDot).join("")
+// //   console.log(fractionalNumber);
+// //   console.log(arrayOfsymbols);
+// //   console.log(arrayOfsymbols.slice(endtDot));
+// //   arrayOfsymbols = arrayOfsymbols.slice(0, startDot + 1).concat(fractionalNumber, arrayOfsymbols.slice(endtDot))
+// //   console.log(arrayOfsymbols);
+
+
+// //   for (var f = 0; f < arrayOfsymbols.length; f++) { // где возможно, привожу к числу
+// //     var t = parseFloat(arrayOfsymbols[f])
+// //     if (!isNaN(t)) {
+// //       arrayOfsymbols[f] = t
+// //     }
+// //     console.log(typeof arrayOfsymbols[f])
+// //   }
+// //   console.log(arrayOfsymbols);
+// //   var start;
+// //   var end;
+// //   for (var i = 0; i < arrayOfsymbols.length; i++) {
+// //     if (arrayOfsymbols[i] === "(") {
+// //       //  start = arrayOfsymbols.slice(i);//отрезаю все до скобки
+// //       start = i + 1
+// //       //   console.log(i)
+// //     }
+// //     if (arrayOfsymbols[i] === ")") {
+// //       // console.log(i)
+// //       //  end = arrayOfsymbols.slice(i);//убираю скобку в начале и отрезаю скобку в конце и один знак
+// //       var end = i;
+// //       //  console.log(end)
+// //     }
+// //   }
+// //   var inBrackets = arrayOfsymbols.slice(start, end)
+// //   //console.log(inBrackets) 
+
+// // }
+// // calc("2*5+(-34+1)+1878.2-63")
+
+
+
+
+
+
+
+// //console.log(calc("2*(-3+1)+1"))
+
+// // var regExp = /\(([^)]+)\)/;
+// // var matches = regExp.exec("2*(-3+1)+1");
+
+// // //matches[1] contains the value between the parentheses
+// // console.log(matches);
+// // console.log(matches[1]);
+
+
+
+
+
+// function calcGlobal(equation) {
+//   //console.log(equation)//
+//   ///////////////////////////////////////////////////////////////////////преобразую в массив
+//   var arrayOfsymbols = equation.split("")
+//   //console.log(arrayOfsymbols);
+//   var newArr = [];
+//   var elementString = "";
+//   ////////////////////////////////////////////  если после скобки идет минус нужно сделать отрицательное число
+//   for (var j = 0; j < arrayOfsymbols.length; j++) {
+//     if (arrayOfsymbols[j] === "(" && (arrayOfsymbols[j + 1]) === "-") {
+//       arrayOfsymbols[j + 1] + arrayOfsymbols[j + 2]
+//       arrayOfsymbols = arrayOfsymbols.slice(0, j + 1).concat(arrayOfsymbols[j + 1] + arrayOfsymbols[j + 2], arrayOfsymbols.slice(j + 3))
+//       //console.log(arrayOfsymbols)
+//     }
+//   }
+//   //////////////////////////////////////////////////////////////////////// где есть точка делаю дробное число
+//   for (var k = 0; k < arrayOfsymbols.length; k++) {
+//     //console.log(arrayOfsymbols[k]);
+//     if (!isNaN(parseFloat(arrayOfsymbols[k])) || arrayOfsymbols[k] === ".") {
+//       elementString += arrayOfsymbols[k]
+//       console.log(elementString);
+
+//     } else {
+//       if (elementString.length !== 0) {
+//         newArr.push(elementString)
+//         newArr.push(arrayOfsymbols[k])
+
+//         elementString = ""
+//       } else {
+//         newArr.push(arrayOfsymbols[k])
+//       }
+//     }
+
+//   }
+//   //console.log(elementString);
+//   //console.log(newArr);
+//   //////////////////////////////////////////////////////////////////////// где возможно, привожу к числу
+//   for (var f = 0; f < newArr.length; f++) {
+//     var t = parseFloat(newArr[f])
+//     if (!isNaN(t)) {
+//       newArr[f] = t
+//       console.log(t);
+//     }
+//     //console.log(typeof newArr[f])
+//   }
+//   console.log(newArr);
+//   //////////////////////////////////////////////////////////////////////  если есть скобки, нахожу что в них
+
+//   for (var i = 0; i < newArr.length; i++) {
+//     var start;
+//     var end;
+//     var newArrPart;
+//     if (newArr[i] === "(") {
+
+//       //  start = arrayOfsymbols.slice(i);//отрезаю все до скобки
+//       start = i + 1
+//       console.log(start)
+//       newArrPart = newArr.slice(0, i)
+//       console.log(newArrPart)
+//     }
+//     if (newArr[i] === ")") {
+//       // console.log(i)
+//       //  end = arrayOfsymbols.slice(i);//убираю скобку в начале и отрезаю скобку в конце и один знак
+//       end = i;
+//       console.log(end)
+//       var inBrackets = newArr.slice(start, end);
+//       console.log(calc(inBrackets));
+//       var inBracketsResult = calc(inBrackets)
+//       newArr = newArrPart.concat(inBracketsResult, newArr.slice(i + 1))
+//     }
+//   }
+//   // var inBrackets = newArr.slice(start, end)
+
+//   console.log(newArr);
+//   console.log(calc(newArr));
+//   calc(newArr)
+
+//   /////////////////////////////////////////////////////////сама функция вычислений
+//   function calc(arrayOfEquations) {
+//     var action;
+//     console.log(arrayOfEquations)
+//     while (arrayOfEquations.length > 1) {
+//       console.log("!!!!")
+//       for (var i = 0; i < arrayOfEquations.length; i++) {
+//         if (arrayOfEquations[i] === "*") {
+//           console.log("!!!!")
+//           action = arrayOfEquations[i - 1] * arrayOfEquations[i + 1];
+//           if (i + 1 === arrayOfEquations.length - 1) {
+//             arrayOfEquations = arrayOfEquations.slice(0, i - 1).concat(action)
+//           } else {
+//             arrayOfEquations = arrayOfEquations.slice(0, i - 1).concat(action, arrayOfEquations.slice([i + 2]))
+//           }
+
+//           console.log(arrayOfEquations)
+//         }
+
+//       }
+
+//       for (var i = 0; i < arrayOfEquations.length; i++) {
+//         if (arrayOfEquations[i] === "/") {
+//           action = arrayOfEquations[i - 1] / arrayOfEquations[i + 1];
+//           if (i + 1 === arrayOfEquations.length - 1) {
+//             arrayOfEquations = arrayOfEquations.slice(0, i - 1).concat(action)
+//           } else {
+//             arrayOfEquations = arrayOfEquations.slice(0, i - 1).concat(action, arrayOfEquations.slice([i + 2]))
+//           }
+//           console.log(arrayOfEquations)
+//         }
+//       }
+
+//       for (var i = 0; i < arrayOfEquations.length; i++) {
+//         if (arrayOfEquations[i] === "+") {
+//           action = arrayOfEquations[i - 1] + arrayOfEquations[i + 1];
+//           if (i + 1 === arrayOfEquations.length - 1) {
+//             arrayOfEquations = arrayOfEquations.slice(0, i - 1).concat(action)
+//           } else {
+//             arrayOfEquations = arrayOfEquations.slice(0, i - 1).concat(action, arrayOfEquations.slice([i + 2]))
+//           }
+//           console.log(arrayOfEquations)
+//         }
+//       }
+//       for (var i = 0; i < arrayOfEquations.length; i++) {
+//         if (arrayOfEquations[i] === "-") {
+//           action = arrayOfEquations[i - 1] - arrayOfEquations[i + 1];
+//           if (i + 1 === arrayOfEquations.length - 1) {
+//             arrayOfEquations = arrayOfEquations.slice(0, i - 1).concat(action)
+//           } else {
+//             arrayOfEquations = arrayOfEquations.slice(0, i - 1).concat(action, arrayOfEquations.slice([i + 2]))
+//           }
+//           console.log(arrayOfEquations)
+//         }
+//       }
+//       //console.log(arrayOfEquations)  
+//       console.log(arrayOfEquations)
+//       return arrayOfEquations
+//     }
+//   }
+// }
+// calcGlobal("2*5+(-34+1)+1878.2")
+// calcGlobal("2*5+(-34+1)+1878.2")
+
 function calc(statement) {
   var prioritet = {  // приоритеты операторов
     "(": 0,
---
+++ FD2/DZ7/CLOCK_DOM/CLOCK_DOM.js
@@ -0,0 +1,128 @@
+"use strict";
+//создаю желтый круг
+var clock = document.getElementById("clock");
+var yellow = document.createElement("div");
+var heightDiv = window.getComputedStyle(clock).height;
+heightDiv = parseFloat(heightDiv.replace(/[px]/g, ''));
+clock.appendChild(yellow);
+//позиционирую желтый круг
+yellow.setAttribute("style", "background: sandybrown;  border-radius: 50%");
+//диаметр желтого круга
+var diam = heightDiv * 0.9
+yellow.style.height = diam + "px";
+yellow.style.width = diam + "px";
+var clockPosition = window.getComputedStyle(yellow).height;
+yellow.style.position = "absolute";
+clockPosition = parseFloat(clockPosition.replace(/[px]/g, ''));
+var distance = (heightDiv / 2 - clockPosition / 2)   //отступы от края контейнера до желтого круга
+yellow.style.left = distance + "px";
+yellow.style.top = distance + "px";
+//нахожу центр желтого круга
+var yellowCenterX = yellow.offsetLeft - distance + yellow.offsetWidth / 2;
+var yellowCenterY = yellow.offsetTop - distance + yellow.offsetHeight / 2;
+var yellowRadius = diam / 2;
+// создаю и позиционирую зеленые круги
+for (var i = 1; i <= 12; i++) {
+    var green = document.createElement("div");
+    yellow.appendChild(green);
+    green.setAttribute("style", "background: lightgreen; border-radius: 50%;  text-align: center;");
+    var greenHeight = heightDiv * 0.09;
+    green.style.height = greenHeight + "px";
+    green.style.width = greenHeight + "px";
+    green.style.position = "absolute";
+    var angle = ((360 / 12) * i) / 180 * Math.PI;
+    var greenCenterX = yellowCenterX + yellowRadius * 0.8 * Math.sin(angle);
+    var greenCenterY = yellowCenterY - yellowRadius * 0.8 * Math.cos(angle);
+    green.style.left = Math.round(greenCenterX - green.offsetWidth / 2) + "px";
+    green.style.top = Math.round(greenCenterY - green.offsetHeight / 2) + "px";
+    // добавляю цифры в кружочки 
+    var numberTime = document.createElement("div");
+    green.appendChild(numberTime);
+    numberTime.innerHTML = i;
+    numberTime.setAttribute("style", "color: black;");
+    var fontSize = heightDiv * 0.04;
+    numberTime.style.fontSize = fontSize + "px";
+    numberTime.style.marginTop = (greenHeight - fontSize) / 2 + "px";
+}
+//создаю секудную стрелку
+var handSecondDiv = document.createElement("div");
+clock.appendChild(handSecondDiv);
+handSecondDiv.style.position = "absolute";
+var handSecond = document.createElement("div");
+handSecondDiv.appendChild(handSecond);
+handSecondDiv.style.height = heightDiv + "px";
+handSecondDiv.style.width = heightDiv + "px";
+handSecond.setAttribute("style", "background: black;  border-radius: 30%");
+var heightHandSecond = yellowRadius * 0.8;
+handSecond.style.height = heightHandSecond + "px";
+var widthtHandSecond = yellowRadius * 0.02;
+handSecond.style.width = widthtHandSecond + "px";
+handSecondDiv.style.zIndex = 11;
+handSecond.style.position = "absolute";//позиционирую стрелку
+handSecond.style.left = (heightDiv - widthtHandSecond) / 2 + "px";
+handSecond.style.top = (yellowRadius - heightHandSecond) + yellowRadius * 0.2 + "px";
+//создаю минутную стрелку
+var handMinuteDiv = document.createElement("div");
+clock.appendChild(handMinuteDiv);
+handMinuteDiv.style.position = "absolute";
+var handMinute = document.createElement("div");
+handMinuteDiv.appendChild(handMinute);
+handMinuteDiv.style.height = heightDiv + "px";
+handMinuteDiv.style.width = heightDiv + "px";
+handMinute.setAttribute("style", "background: black; border-radius: 30%");
+var heightHandMinute = yellowRadius * 0.7;
+handMinute.style.height = heightHandMinute + "px";
+var widthtHandMinute = yellowRadius * 0.03;
+handMinute.style.width = widthtHandMinute + "px";
+handMinuteDiv.style.zIndex = 10;
+handMinute.style.position = "absolute";//позиционирую стрелку
+handMinute.style.left = (heightDiv - widthtHandMinute) / 2 + "px";
+handMinute.style.top = (yellowRadius - heightHandMinute) + yellowRadius * 0.2 + "px";
+//создаю часовую стрелку
+var handHourDiv = document.createElement("div");
+clock.appendChild(handHourDiv);
+handHourDiv.style.position = "absolute";
+var handHour = document.createElement("div");
+handHourDiv.appendChild(handHour);
+handHourDiv.style.height = heightDiv + "px";
+handHourDiv.style.width = heightDiv + "px";
+handHour.setAttribute("style", "background: black; border-radius: 30%");
+var heightHandHour = yellowRadius * 0.5;
+handHour.style.height = heightHandHour + "px";
+var widthtHandHour = yellowRadius * 0.06;
+handHour.style.width = widthtHandHour + "px";
+handHour.style.position = "absolute";//позиционирую стрелку
+handHour.style.left = (heightDiv - widthtHandHour) / 2 + "px";
+handHour.style.top = (yellowRadius - heightHandHour) + yellowRadius * 0.2 + "px";
+/////////
+//электронные часы
+var electronicClock = document.createElement("div");
+clock.appendChild(electronicClock);
+var electronicClockHeight = heightDiv * 0.1;
+var electronicClockWidth = heightDiv * 0.3;
+electronicClock.setAttribute("style", `text-align: center; font-size: ${heightDiv * 0.1}px`);
+electronicClock.style.position = "absolute";
+//electronicClock.setAttribute("style", "text-align: center;");
+electronicClock.style.height = electronicClockHeight + "px";
+electronicClock.style.width = electronicClockWidth + "px";
+electronicClock.style.top = (heightDiv - electronicClockHeight) / 3 + "px";
+electronicClock.style.left = (heightDiv / 2 - electronicClockWidth / 2) + "px";
+setInterval(() => {
+    var time = new Date();
+    // console.log(time)
+    var hoursScore = time.getHours();
+    var minutesScore = time.getMinutes();
+    var secondsScore = time.getSeconds();
+    var hours = hoursScore * (360 / 12);
+    var minutes = minutesScore * (360 / 60);
+    var seconds = secondsScore * (360 / 60);
+    handHourDiv.style.transform = `rotateZ(${(hours) + (minutes / 12)}deg)`;
+    handMinuteDiv.style.transform = `rotateZ(${minutes}deg)`;
+    handSecondDiv.style.transform = `rotateZ(${seconds}deg)`;
+    if (hoursScore < 10) { hoursScore = "0" + hoursScore }
+    if (minutesScore < 10) { minutesScore = "0" + minutesScore }
+    if (secondsScore < 10) { secondsScore = "0" + secondsScore }
+    electronicClock.innerHTML = `${hoursScore}:${minutesScore}:${secondsScore}`;
+    console.log(electronicClock.innerHTML)
+    electronicClock.style.zIndex = 10;
+}, 1000)
