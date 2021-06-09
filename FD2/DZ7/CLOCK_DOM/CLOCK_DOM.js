d42fe98b2f1d9b1d5a30302a975716079d0ed50f
mOstanina <maryiaost@gmail.com>
2021-06-10 00:25:17 +0300

    clock_dom-2

--- FD2/DZ7/CLOCK_DOM/CLOCK_DOM.js
+++ FD2/DZ7/CLOCK_DOM/CLOCK_DOM.js
@@ -61,6 +61,13 @@ handSecondDiv.style.zIndex = 11;
 handSecond.style.position = "absolute";//позиционирую стрелку
 handSecond.style.left = (heightDiv - widthtHandSecond) / 2 + "px";
 handSecond.style.top = (yellowRadius - heightHandSecond) + yellowRadius * 0.2 + "px";
+//
+var timeAnalog = new Date();
+//
+var secondsScore = timeAnalog.getSeconds();
+var seconds = secondsScore * (360 / 60);
+handSecondDiv.style.transform = `rotateZ(${seconds}deg)`;
+//
 //создаю минутную стрелку
 var handMinuteDiv = document.createElement("div");
 clock.appendChild(handMinuteDiv);
@@ -78,6 +85,11 @@ handMinuteDiv.style.zIndex = 10;
 handMinute.style.position = "absolute";//позиционирую стрелку
 handMinute.style.left = (heightDiv - widthtHandMinute) / 2 + "px";
 handMinute.style.top = (yellowRadius - heightHandMinute) + yellowRadius * 0.2 + "px";
+//
+var minutesScore = timeAnalog.getMinutes();
+var minutes = minutesScore * (360 / 60);
+handMinuteDiv.style.transform = `rotateZ(${minutes}deg)`;
+//
 //создаю часовую стрелку
 var handHourDiv = document.createElement("div");
 clock.appendChild(handHourDiv);
@@ -94,7 +106,11 @@ handHour.style.width = widthtHandHour + "px";
 handHour.style.position = "absolute";//позиционирую стрелку
 handHour.style.left = (heightDiv - widthtHandHour) / 2 + "px";
 handHour.style.top = (yellowRadius - heightHandHour) + yellowRadius * 0.2 + "px";
-/////////
+//
+var hoursScore = timeAnalog.getHours();
+var hours = hoursScore * (360 / 12);
+handHourDiv.style.transform = `rotateZ(${(hours) + (minutes / 12)}deg)`;
+//
 //электронные часы
 var electronicClock = document.createElement("div");
 clock.appendChild(electronicClock);
@@ -107,9 +123,14 @@ electronicClock.style.height = electronicClockHeight + "px";
 electronicClock.style.width = electronicClockWidth + "px";
 electronicClock.style.top = (heightDiv - electronicClockHeight) / 3 + "px";
 electronicClock.style.left = (heightDiv / 2 - electronicClockWidth / 2) + "px";
+//
+electronicClock.innerHTML = `${hoursScore}:${minutesScore}:${secondsScore}`;
+if (hoursScore < 10) { hoursScore = "0" + hoursScore }
+if (minutesScore < 10) { minutesScore = "0" + minutesScore }
+if (secondsScore < 10) { secondsScore = "0" + secondsScore }
+//
 setInterval(() => {
     var time = new Date();
-    // console.log(time)
     var hoursScore = time.getHours();
     var minutesScore = time.getMinutes();
     var secondsScore = time.getSeconds();
