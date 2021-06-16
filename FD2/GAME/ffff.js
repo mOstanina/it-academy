26b7759348edd57d01a2a8a22d488731d14b8634
mOstanina <maryiaost@gmail.com>
2021-06-16 22:59:48 +0300

    g-2.1

--- FD2/GAME/ffff.js
+++ FD2/GAME/ffff.js
@@ -1,7 +1,7 @@
 // создаю фон для ирового поля
 var gameWindow = document.getElementById("gameWindow");
 var infoHeight = document.getElementById("info");
-gameWindow.setAttribute("background", "url(//)")
+// gameWindow.setAttribute("background", "url(//)")
 screen.addEventListener("orientationchange", function () {
     console.log("The orientation of the screen is: " + screen.orientation);
 });
