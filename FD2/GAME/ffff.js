b6f44146e8a91adcd888160e19ef8e8087d6e480
mOstanina <maryiaost@gmail.com>
2021-06-16 22:52:35 +0300

    g-22

--- FD2/GAME/ffff.js
+++ FD2/GAME/ffff.js
@@ -4,4 +4,4 @@ var infoHeight = document.getElementById("info");
 gameWindow.setAttribute("background", "url(//)")
 screen.addEventListener("orientationchange", function () {
     console.log("The orientation of the screen is: " + screen.orientation);
-  });
+});
