acadaa4692aa847553062b1ef5a1658fda86567b
mOstanina <maryiaost@gmail.com>
2021-06-16 21:44:25 +0300

    g-2

---
+++ FD2/GAME/ffff.js
@@ -0,0 +1,4 @@
+// создаю фон для ирового поля
+var gameWindow = document.getElementById("gameWindow");
+var infoHeight = document.getElementById("info");
+gameWindow.setAttribute("background", "url(//)")
