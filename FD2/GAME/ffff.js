dd4dae64a5974efd4127608827b3bbb86dc78646
mOstanina <maryiaost@gmail.com>
2021-06-17 23:18:01 +0300

    g-4

--- FD2/GAME/ffff.js
+++ FD2/GAME/ffff.js
@@ -4,10 +4,10 @@ var gameScreen = document.getElementById("screen");// нахожу весь эк
 var gameWindow = document.getElementById("gameWindow");//нахожу игровое пространство
 var infoHeight = document.getElementById("info");//нахожу область отображения счета
 var widthGameWindow = window.getComputedStyle(gameWindow).width;// определяю ширину игровой области
-widthGameWindow = parseFloat(widthGameWindow.replace(/[px]/g, ''))
+widthGameWindow = parseFloat(widthGameWindow.replace(/[px]/g, ''));
 console.log(widthGameWindow);
-heightGameWindow = widthGameWindow * 0.5
-gameWindow.setAttribute("style", "height:" + heightGameWindow + "px")
+heightGameWindow = widthGameWindow * 0.5;
+gameWindow.setAttribute("style", "height:" + heightGameWindow + "px");
 
 var message = document.createElement("div");// сообщение повернуть экран
 message.setAttribute("class", "mes");
@@ -21,25 +21,22 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
         console.log(mobileScreenHeight)
         var mobileScreenWidth = window.screen.width//определяю ширину экрана устройства
         console.log(mobileScreenWidth)
-    
-        if (widthGameWindow > mobileScreenWidth) {
-            widthGameWindow = mobileScreenWidth
-        }
-        if (heightGameWindow > mobileScreenHeight) {
-            heightGameWindow = mobileScreenHeight
-            console.log(heightGameWindow)
-        }
+        gameWindow.setAttribute("style", "height:" + mobileScreenHeight)
+        gameWindow.setAttribute("style", "width:" + mobileScreenWidth)
+   
     }
-    sizeMobile()
+    sizeMobile();
+    console.log(window.screen.width)
+    
     function resize() {
-        console.log("!!!!")
+        console.log("resize")
         var scrollHeight = Math.max(
             document.body.scrollHeight, document.documentElement.scrollHeight,
             document.body.offsetHeight, document.documentElement.offsetHeight,
             document.body.clientHeight, document.documentElement.clientHeight
         );
         gameWindow.setAttribute("style", "height:" + scrollHeight + "px")
-        console.log(scrollHeight)
+       console.log(scrollHeight)
     }
     resize()
     console.log("Вы используете мобильное устройство (телефон или планшет).")
@@ -52,30 +49,25 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
     // alert( "Текущая прокрутка: " + scrollTop );
     // alert( window.innerHeight ); // вся высота окна
     // alert( document.documentElement.clientHeight ); // высота минус прокрутка
+    
     window.addEventListener("orientationchange", function (event) {
         resize()
         var orientationAngle = event.target.screen.orientation.angle
         console.log("the orientation of the device is now " + orientationAngle);
         console.log("height" + heightGameWindow);
         console.log("width" + widthGameWindow);
-
         console.log(message)
         if (orientationAngle === 0) {
             console.log("3333")
             gameContainer.removeChild(gameScreen)
             gameContainer.appendChild(message)
-
-
         } else {
             if (message) {
                 gameContainer.removeChild(message);
                 gameContainer.appendChild(gameScreen)
             }
-
         }
     });
-
-
 } else {
     console.log("Вы используете ПК.")
 }
