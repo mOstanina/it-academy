1988d8b900d9288b8db3e2103cca6bd2fa68f599
mOstanina <maryiaost@gmail.com>
2021-07-06 12:33:59 +0300

    dz-10-3

--- FD2/DZ10/DRINKS_AJAX_STORAGE.js
+++ FD2/DZ10/DRINKS_AJAX_STORAGE.js
@@ -1,43 +1,101 @@
 "use strict"
-function LocStorage(nameLocalStorage) {
+var ajaxHandlerScript = "https://fe.it-academy.by/AjaxStringStorage2.php";
+var updatePassword;
+var stringName = "OSTANINA_DRINKS_AJAX_STORAGE"
+
+function addInfo(storage) {
+    //// updatePassword = Math.random();
+    $.ajax({
+        url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
+        data: { f: "INSERT", n: stringName, v: JSON.stringify(storage) },
+        success: lockGetReady, error: errorHandler
+    }
+    );
+}
+
+function storeInfo(storage) {
+    updatePassword = Math.random();
+    $.ajax({
+        url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
+        data: { f: 'LOCKGET', n: stringName, p: updatePassword },
+        success: lockGetReady, error: errorHandler
+    }
+    );
+}
+
+function lockGetReady(callresult, storage) {
+    if (callresult.error != undefined)
+        alert(callresult.error);
+    else {
+        $.ajax({
+            url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
+            data: { f: 'UPDATE', n: stringName, v: JSON.stringify(storage), p: updatePassword },
+            success: updateReady, error: errorHandler
+        }
+        );
+    }
+}
+
+function updateReady(callresult) {
+    if (callresult.error != undefined)
+        alert(callresult.error);
+}
+
+function restoreInfo() {
+    $.ajax(
+        {
+            url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
+            data: { f: 'READ', n: stringName },
+            success: readReady, error: errorHandler
+        }
+    );
+}
+
+function readReady(callresult) {
+    if (callresult.error != undefined)
+        alert(callresult.error);
+    else if (callresult.result != "") {
+        var info = JSON.parse(callresult.result);
+        return info
+    }
+}
+
+function errorHandler(jqXHR, statusStr, errorStr) {
+    alert(statusStr + " " + errorStr);
+}
+
+function AJAXStorage() {
     var self = this;
-    if(nameLocalStorage in localStorage){
-        self.storage=JSON.parse(localStorage.getItem(nameLocalStorage))
-    }else{
     self.storage = {};
-    localStorage.setItem(nameLocalStorage, JSON.stringify(self.storage))
-    };
+    addInfo(self.storage)
+
+
     self.addValue = function (key, value) {
+
         self.storage[key] = value;
-        // console.log(self.storage); //для проверки
-        // console.log(Object.keys(self.storage)); //для проверки
-        localStorage.setItem(nameLocalStorage, JSON.stringify(self.storage))
+        storeInfo(self.storage)
     };
     self.getValue = function (key) {
-        // console.log(self.storage[key]);//для проверки
-        // console.log(typeof(self.storage[key]));//для проверки
+        restoreInfo()
         return (self.storage[key]);
     };
     self.deleteValue = function (key) {
         if (key in self.storage) {
             delete self.storage[key];
-            localStorage.setItem(nameLocalStorage, JSON.stringify(self.storage))
-            // console.log(true);//для проверки
-            // console.log(Object.keys(self.storage)); //для проверки
+            storeInfo(self.storage)
             return true;
         } else {
-            // console.log(false);//для проверки
             return false;
         }
     };
     self.getKeys = function () {
         var claearArrayOfKeys = Object.keys(self.storage);
-        // console.log(claearArrayOfKeys);//для проверки
+        restoreInfo()
         return claearArrayOfKeys;
     };
-    
 };
-var drinkStorage = new LocStorage("drink");
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+var drinkStorage = new AJAXStorage();
 var alco;
 var recipe;
 var nameDrink;
@@ -47,7 +105,6 @@ function addDrink() {
     var c = (a == true) ? "да" : "нет";
     var b = prompt("введите рецепт напитка");
     var hash = { alco: c, recipe: b }
-    // drinkStorage.addValue(nameDrink, (drinkStorage.storage[nameDrink] = { alco: c, recipe: b }));
     drinkStorage.addValue(nameDrink, hash);
 };
 function infoAboutDrink() {
@@ -55,47 +112,14 @@ function infoAboutDrink() {
     var drinkInfoMessage = drinkStorage.getValue(nameOfDrink);
     if (typeof drinkInfoMessage == "object") {
         alert("напиток: " + nameOfDrink + "\n" + "алкогольный: " + drinkInfoMessage["alco"] + "\n" + "рецепт приготовления: " + drinkInfoMessage["recipe"]);
-    } else{
+    } else {
         alert("такого напитка нет в перечне");
     };
 };
 function deleteDrink() {
     var presenceInList = drinkStorage.deleteValue((prompt("введите название напитка, который хотите удалить")));
     (presenceInList) ? alert("напиток удален") : alert("такого напитка нет в перечне");
-
 };
 function sowAllDrinks() {
     alert(drinkStorage.getKeys());
 };
-////////
-var dishesStorage = new LocStorage("dishes");
-var calories;
-var recipe;
-var nameDishes;
-function addDish() {
-    nameDishes = prompt("введите название блюда");
-    var a = confirm("блюдо очень калорийное?");
-    var c = (a == true) ? "да" : "нет";
-    var b = prompt("введите рецепт блюда");
-    var hash = { calories: c, recipe: b }
-    // dishesStorage.addValue(nameDishes, (dishesStorage.storage[nameDishes] = { calories: c, recipe: b }));
-    dishesStorage.addValue(nameDishes, hash);
-};
-function infoAboutDishes() {
-    var nameOfDish = prompt("введите название блюда");
-    var dishInfoMessage = dishesStorage.getValue(nameOfDish);
-    if (typeof dishInfoMessage == "object") {
-        alert("блюда: " + nameOfDish + "\n" + "калорийность: " + dishInfoMessage["calories"] + "\n" + "рецепт приготовления: " + dishInfoMessage["recipe"]);
-    } else{
-        alert("такого блюда нет в перечне");
-    };
-};
-function deleteDishes() {
-    var presenceInList = dishesStorage.deleteValue((prompt("введите название блюда, который хотите удалить")));
-    (presenceInList) ? alert("блюда удалено") : alert("такого блюда нет в перечне");
-
-};
-function sowAllDishes() {
-    alert(dishesStorage.getKeys());
-};
-
