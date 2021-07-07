812f67afebdfd616c5a73fd8ebb0832ea9a39691
mOstanina <maryiaost@gmail.com>
2021-07-07 10:03:43 +0300

    dz-10-6

--- FD2/DZ10/DRINKS_AJAX_STORAGE.js
+++ FD2/DZ10/DRINKS_AJAX_STORAGE.js
@@ -1,76 +1,64 @@
 "use strict"
 var ajaxHandlerScript = "https://fe.it-academy.by/AjaxStringStorage2.php";
 var updatePassword;
-var stringName = "OSTANINA_DRINKS_AJAX_STORA"
-
-function storeInfo() {
-    updatePassword = Math.random();
-    $.ajax({
-        url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
-        data: { f: 'LOCKGET', n: stringName, p: updatePassword },
-        success: lockGetReady, error: errorHandler
-    });
-}
-var storage;
-function lockGetReady(callresult) {
-    if (callresult.error != undefined)
-        alert(callresult.error);
-    else {
-        $.ajax({
-            url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
-            data: { f: 'UPDATE', n: stringName, v: JSON.stringify(storage), p: updatePassword },
-            success: updateReady, error: errorHandler
-        });
-    }
-}
-
-function updateReady(callresult) {
-    if (callresult.error != undefined) {
-        alert(callresult.error);
-    }
-}
-
-function restoreInfo() {
-    $.ajax({
-        url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
-        data: { f: 'READ', n: stringName },
-        success: readReady, error: errorHandler
-    });
-}
-
-function readReady(callresult) {
-    if (callresult.error != undefined)
-        alert(callresult.error);
-    else if (callresult.result != "") {
-        var info = JSON.parse(callresult.result);
-        return info
-    }
-}
-
-function errorHandler(jqXHR, statusStr, errorStr) {
-    alert(statusStr + " " + errorStr);
-}
-
 function AJAXStorage() {
     var self = this;
-    storage = self.storage = {};
-
+    self.stringName = stringName
+    self.storage;
     // addInfo(self.storage)
-    //  restoreInfo()
-
+    self.restoreInfo = function (stringName) {
+        $.ajax({
+            url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
+            data: { f: 'READ', n: stringName },
+            success: readReady, error: errorHandler
+        });
+    }
+    function readReady(callresult) {
+        if (callresult.error != undefined)
+            alert(callresult.error);
+        else if (callresult.result != "") {
+            self.storage = JSON.parse(callresult.result);
+            //return info
+            //storage = self.storage
+        }
+    }
+    self.storeInfo = function (stringName) {
+        updatePassword = Math.random();
+        $.ajax({
+            url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
+            data: { f: 'LOCKGET', n: stringName, p: updatePassword },
+            success: lockGetReady, error: errorHandler
+        });
+    }
+    function lockGetReady(callresult) {
+        if (callresult.error != undefined)
+            alert(callresult.error);
+        else {
+            $.ajax({
+                url: ajaxHandlerScript, type: "POST", cache: false, dataType: "json",
+                data: { f: 'UPDATE', n: stringName, v: JSON.stringify(self.storage), p: updatePassword },
+                success: updateReady, error: errorHandler
+            });
+        }
+    }
+    
+    function updateReady(callresult) {
+        if (callresult.error != undefined) {
+            alert(callresult.error);
+        }
+    }
+    function errorHandler(jqXHR, statusStr, errorStr) {
+        alert(statusStr + " " + errorStr);
+    }
     self.addValue = function (key, value) {
-
         self.storage[key] = value;
-        storeInfo(self.storage)
     };
     self.getValue = function (key) {
-        restoreInfo()
         return (self.storage[key]);
     };
     self.deleteValue = function (key) {
         if (key in self.storage) {
             delete self.storage[key];
-            storeInfo(self.storage)
             return true;
         } else {
             return false;
@@ -78,22 +66,31 @@ function AJAXStorage() {
     };
     self.getKeys = function () {
         var claearArrayOfKeys = Object.keys(self.storage);
-        restoreInfo()
         return claearArrayOfKeys;
     };
 };
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 var drinkStorage = new AJAXStorage();
+var stringName = "OSTANINA_DRINKS_AJAX_STORA"
 var alco;
 var recipe;
 var nameDrink;
+///
+var a;
+var c;
+var b;
+var hash = { alco: c, recipe: b }
+///
+window.onload = drinkStorage.restoreInfo(stringName)
+
 function addDrink() {
     nameDrink = prompt("введите название напитка");
-    var a = confirm("напиток алкогольный?");
-    var c = (a == true) ? "да" : "нет";
-    var b = prompt("введите рецепт напитка");
-    var hash = { alco: c, recipe: b }
+    a = confirm("напиток алкогольный?");
+    c = (a == true) ? "да" : "нет";
+    b = prompt("введите рецепт напитка");
+    hash = { alco: c, recipe: b }
     drinkStorage.addValue(nameDrink, hash);
+    drinkStorage.storeInfo(stringName);
 };
 function infoAboutDrink() {
     var nameOfDrink = prompt("введите название напитка");
@@ -107,6 +104,7 @@ function infoAboutDrink() {
 function deleteDrink() {
     var presenceInList = drinkStorage.deleteValue((prompt("введите название напитка, который хотите удалить")));
     (presenceInList) ? alert("напиток удален") : alert("такого напитка нет в перечне");
+    drinkStorage.storeInfo(stringName);
 };
 function sowAllDrinks() {
     alert(drinkStorage.getKeys());
