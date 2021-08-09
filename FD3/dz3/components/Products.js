0a9e77d68eac42d0e580698d677bfc0ec298c6c2
mOstanina <maryiaost@gmail.com>
2021-08-09 14:04:06 +0300

    ishop3 mb complited

--- FD3/dz3/components/Products.js
+++ FD3/dz3/components/Products.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 class Products extends React.Component {
     static propTypes = {
+        workMode: PropTypes.number,
         productName: PropTypes.string.isRequired,
         code: PropTypes.number.isRequired,
         price: PropTypes.number.isRequired,
@@ -16,17 +17,27 @@ class Products extends React.Component {
         cssClassNotSelect: PropTypes.string.isRequired,
         cssClassSelect: PropTypes.string.isRequired,
         cbClickedEdit: PropTypes.func.isRequired,// колбэк для изменения продукта
+        isProductCardChanged: PropTypes.bool.isRequired,
     };
+    state = {
+        isCardChanged: this.props.isProductCardChanged,
+    }
     productClickedForDelete = (EO) => {
         EO.stopPropagation();
-        this.props.cbDelete(this.props.code);
+        if (this.props.isProductCardChanged === false && this.props.workMode !== 3) {//если в форму бвли внесены изменения, клик не будет выполнятся
+            this.props.cbDelete(this.props.code);
+        }
     }
     productClicked = (EO) => {
-        this.props.cbClicked(this.props.code);
+        if (this.props.isProductCardChanged === false && this.props.workMode !== 3) {//если в форму бвли внесены изменения, клик не будет выполнятся
+            this.props.cbClicked(this.props.code);
+        }
     }
     editClicked = (EO) => {
         EO.stopPropagation();
-        this.props.cbClickedEdit(this.props.code);
+        if (this.props.isProductCardChanged === false && this.props.workMode !== 3) {//если в форму бвли внесены изменения, клик не будет выполнятся
+            this.props.cbClickedEdit(this.props.code);
+        }
     }
     render() {
         if (this.props.code !== this.props.clickedProduct) {
@@ -48,7 +59,7 @@ class Products extends React.Component {
                     <div className='url'>{this.props.url}</div>
                     <div className='Count'>{this.props.count}</div>
                     <input type='button' value='edit' onClick={this.editClicked} />
-                    <input type='button' value='delete' onClick={this.productClickedForDelete} />
+                    <input type='button' value='delete' onClick={this.productClickedForDelete} disabled={this.props.isProductCardChanged} />
                 </div>
             );
         }
--- FD3/dz3/components/Shop.js
+++ FD3/dz3/components/Shop.js
@@ -26,56 +26,100 @@ class Shop extends React.Component {
         classNameForClick: 'Product red',
         classNameWithoutClick: 'Product',
         workMode: null,
+        isProductCardChanged: false,
+        btnView: "btnNewProduct",
     }
 
     productClicked = (code) => {//колбэк клика по строке с товаром, подсвечивается строка и отображается карточка товара
         console.log('выбран ответ с кодом ' + code);
         this.setState({ selectedProductCod: code });
         if (this.state.workMode === null) {
-            this.setState({ workMode: 1 });//карточка переходит в режим просмотра
+            this.setState({
+                workMode: 1,  //карточка начинает отображаться в режиме просмотра
+                btnView: "btnNewProduct",//отображаю с помощью CSS-каласса  кнопку добавления нового продукта
+            });
         }
         if (this.state.workMode === 2) {
-            this.setState({ workMode: 1 });//карточка переходит в режим просмотра если вдруг до этого был режим редактирования
+            this.setState({
+                workMode: 1,  //карточка начинает отображаться в режиме просмотра
+                btnView: "btnNewProduct",//отображаю с помощью CSS-каласса  кнопку добавления нового продукта
+            });
         }
     }
 
     productSelectedForDelete = (code) => {// колбэк удаления товара
         console.log('удален товар с кодом ' + code);
         if (code === this.state.selectedProductCod) {
-            this.setState({ selectedProductCod: null });
-            this.setState({ workMode: null });
+            this.setState({
+                selectedProductCod: null, //при удалении товара сбрасываю значение выбранного товара если он же и был выбран
+                workMode: null,           //при удалении товара сбрасываю значение отображения крточки ведь этот товар удален
+                btnView: "btnNewProduct",//если товар удален в режиме кратрочки редактирования или добавления , возвращаю кнопку добавления нового продукта
+            });
         }
         this.setState({
             productsArray: this.state.productsArray.filter(product => product.code !== code)// фильтрую список товара без товара с удаляемым кодом
         });
     }
-    editClicked = (code) => {// колбэк редактрования товараб при клике на edit карточка переходит в режим редактирования
-        console.log(this.state.workMode);
+    editClicked = (code) => {// колбэк редактрования товараб при клике на "edit" карточка переходит в режим редактирования
         console.log('изменеинию подлежит товар с кодом:  ' + code);
-        this.setState({ selectedProductCod: code });
-        this.setState({ workMode: 2 });//карточка переходит в режим редактирования
-        console.log(this.state.workMode);
+        this.setState({
+            selectedProductCod: code,  //  передаю код выбранного товара
+            workMode: 2,   //карточка переходит в режим редактирования
+            btnView: "btnNotView" // "скрываю" с помощью CSS-каласса кнопку добавления нового товара
+        });
     }
-    productCangeInfo = (code, returnArray) => { // колбэк редактрорования товара
+    addNewProd = (EO) => {// функция перевода карточки в режим добавления нового  товара
+        this.setState({
+            workMode: 3,   //карточка переходит в режим редактирования
+            selectedProductCodToDelete: null,
+            selectedProductCod: null,
+            btnView: "btnNotView" // "скрываю" с помощью CSS-каласса кнопку добавления нового товара
+        });
+    }
+    productCangeInfo = (code, returnArray) => { // колбэк добавления товара по кнопке "save"
         console.log('изменеинию подлежит товар с1 кодом:  ' + code);
-        console.log(this.state.productsArray);
         var arr = this.state.productsArray;
-        var newArr=[];
+        var newArr = [];
         arr.forEach(function (item, i, arr) {  // формирую новый массив с товарами
             if (item.code !== code) {
                 newArr.push(item)    // если код товара не совпадает просто добавляю его в новый массив
-            }else{
+            } else {
                 newArr.push(returnArray) // если код товара совпадает  добавляю новый товар в новый массив
             }
         })
+        if (code > this.state.productsArray.length) {
+            newArr.push(returnArray)
+        }
+        console.log(this.state.productsArray)
         this.setState({
-            productsArray: newArr
+            productsArray: newArr,
+            isProductCardChanged: false, // разблокирую клики по строчкам и кнопкам delete и edit
+            workMode: null,
+            btnView: "btnNewProduct" // "возвращаю" с помощью CSS-каласса кнопку добавления нового товара
+        });
+    }
+    changeWorkMode = () => {   //колбэк для кнопки сброса возвращает карточку в режим просмотра и разблокирует кнопки delete 
+        this.setState({
+            workMode: 1,
+            isProductCardChanged: false,
+            btnView: "btnNewProduct" // "возвращаю" с помощью CSS-каласса кнопку добавления нового товара  (!!!!!!!)
         });
     }
 
+    isProductCardChangedFunc = () => {  //  колбэк блокирующий кнопки delete если в карточку были внесены изменения
+        this.setState({ isProductCardChanged: true });
+    }
+    returnWorkMode = () => {  //  колбэк отменяющий добавление нового товара
+        this.setState({
+            workMode: null,
+            //isProductCardChanged: false,
+            btnView: "btnNewProduct" // "возвращаю" с помощью CSS-каласса кнопку добавления нового товара
+        });
+    }
     render() {
         var productsCode = this.state.productsArray.map(v =>
             <Products
+                workMode={this.state.workMode}
                 key={v.code}
                 productName={v.productName}
                 price={v.price}
@@ -89,9 +133,10 @@ class Shop extends React.Component {
                 cssClassNotSelect='Product'
                 cssClassSelect='Product red'
                 cbClickedEdit={this.editClicked}
+                isProductCardChanged={this.state.isProductCardChanged}
             />
         );
-        if (this.state.selectedProductCod === null) {
+        if (this.state.workMode === null) {
             return (
                 <div className='iShop'>
                     <div className='Product'>
@@ -102,9 +147,12 @@ class Shop extends React.Component {
                         <div className='control'>Control</div>
                     </div>
                     <div className='Products'>{productsCode}</div>
+                    <div className={this.state.btnView}>
+                        <input type='button' value='new product' onClick={this.addNewProd} />
+                    </div>
                 </div>
             );
-        } else {
+        } else if (this.state.workMode === 1 || this.state.workMode === 2) {
             return (
                 <div>
                     <div className='iShop'>
@@ -116,8 +164,13 @@ class Shop extends React.Component {
                             <div className='control'>Control</div>
                         </div>
                         <div className='Products'>{productsCode}</div>
+                        <div className={this.state.btnView}>
+                            <input type='button' value='new product' onClick={this.addNewProd} />
+                        </div>
                     </div>
                     <Card workMode={this.state.workMode}
+                        cbIsProductCardChanged={this.isProductCardChangedFunc}
+                        cbChangeWorkMode={this.changeWorkMode}
                         key={this.state.selectedProductCod}
                         productName={(this.state.productsArray.filter(product => product.code === this.state.selectedProductCod))[0].productName}
                         price={(this.state.productsArray.filter(product => product.code === this.state.selectedProductCod))[0].price}
@@ -125,9 +178,42 @@ class Shop extends React.Component {
                         url={(this.state.productsArray.filter(product => product.code === this.state.selectedProductCod))[0].url}
                         count={(this.state.productsArray.filter(product => product.code === this.state.selectedProductCod))[0].count}
                         cbChangeProductInfo={this.productCangeInfo}
+                        errorMessage={"transparent"}
                     />
                 </div>
             );
+        } else if (this.state.workMode === 3) {
+            return (
+                <div>
+                    <div className='iShop'>
+                        <div className='Product'>
+                            <div className='ProductName'>ProductName</div>
+                            <div className='price'>Price</div>
+                            <div className='url'>URL</div>
+                            <div className='Count'>Quality</div>
+                            <div className='control'>Control</div>
+                        </div>
+                        <div className='Products'>{productsCode}</div>
+                        <div className={this.state.btnView}>
+                            <input type='button' value='new product' onClick={this.addNewProd} />
+                        </div>
+                    </div>
+                    <Card workMode={3}
+                        cbIsProductCardChanged={this.isProductCardChangedFunc}
+                        cbChangeWorkMode={this.changeWorkMode}
+                        key={(this.props.products).length + 1}
+                        productName={""}
+                        price={0}
+                        code={this.props.products.length + 1}
+                        url={""}
+                        count={0}
+                        cbChangeProductInfo={this.productCangeInfo}
+                        cbReturnWorkMode={this.returnWorkMode}
+                        errorMessage={"error"}
+                    />
+                </div>
+            );
+
         }
     }
 }
