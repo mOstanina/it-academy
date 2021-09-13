492999691851d3ad586239b41c955bd2cd87d04a
mOstanina <maryiaost@gmail.com>
2021-09-13 18:11:29 +0300

    dz-6-tests-2

--- FD3/dz-6-tests/Mobile/__tests__/toSave-test.js
+++ FD3/dz-6-tests/Mobile/__tests__/toSave-test.js
@@ -3,37 +3,31 @@
 import { toSave } from '../modules/toSave';
 
 test('проверка сохранения клиентов', () => {
+  const arr1 = [{ id: 101, fam: "Иванов", im: "Иван", otch: "Иванович", balance: 200 },
+  { id: 105, fam: "Сидоров", im: "Сидор", otch: "Сидорович", balance: 250 },
+  { id: 110, fam: "Петров", im: "Пётр", otch: "Петрович", balance: 180 },
+  { id: 120, fam: "Григорьев", im: "Григорий", otch: "Григорьевич", balance: -220 }]
 
-  expect(toSave(
-    { fam: "ИвановВВВВ", im: "Иван", otch: "Иванович", balance: 200},
-    [
-      { id: 101, fam: "Иванов", im: "Иван", otch: "Иванович", balance: 200 },
-      { id: 105, fam: "Сидоров", im: "Сидор", otch: "Сидорович", balance: 250 },
-      { id: 110, fam: "Петров", im: "Пётр", otch: "Петрович", balance: 180 },
-      { id: 120, fam: "Григорьев", im: "Григорий", otch: "Григорьевич", balance: -220 },
-    ],
-    2
-  )).toBe([
-    { id: 101, fam: "ИвановВВВВ", im: "Иван", otch: "Иванович", balance: 200 },
-    { id: 110, fam: "Петров", im: "Пётр", otch: "Петрович", balance: 180 },
-    { id: 120, fam: "Григорьев", im: "Григорий", otch: "Григорьевич", balance: -220 },
-    { id: 130, fam: "Савельев", im: "Савелий", otch: "Савельевич", balance: 800 },
-  ]);
+  const clientModified = {id: 101, fam: "ИвановВВВВ", im: "Иван", otch: "Иванович", balance: 200 }
 
-  expect(toSave(
-    { fam: "Савельев", im: "Савелий", otch: "Савельевич", balance: 800 },
-    [
-      { id: 101, fam: "Иванов", im: "Иван", otch: "Иванович", balance: 200 },
-      { id: 105, fam: "Сидоров", im: "Сидор", otch: "Сидорович", balance: 250 },
-      { id: 110, fam: "Петров", im: "Пётр", otch: "Петрович", balance: 180 },
-      { id: 120, fam: "Григорьев", im: "Григорий", otch: "Григорьевич", balance: -220 },
-    ],
-    2
-  )).toBe([
-    { id: 101, fam: "Иванов", im: "Иван", otch: "Иванович", balance: 200 },
-    { id: 110, fam: "Петров", im: "Пётр", otch: "Петрович", balance: 180 },
-    { id: 120, fam: "Григорьев", im: "Григорий", otch: "Григорьевич", balance: -220 },
-    { id: 130, fam: "Савельев", im: "Савелий", otch: "Савельевич", balance: 800 },
-  ]);
+  expect(toSave(clientModified, arr1, 1)).toEqual([clientModified].concat(arr1.slice(1)));
+  expect(toSave(clientModified, arr1, 2)).toEqual([...arr1, { id: 130, fam: "ИвановВВВВ", im: "Иван", otch: "Иванович", balance: 200 }]);
+
+
+  // expect(toSave(
+  //   { fam: "Савельев", im: "Савелий", otch: "Савельевич", balance: 800 },
+  //   [
+  //     { id: 101, fam: "Иванов", im: "Иван", otch: "Иванович", balance: 200 },
+  //     { id: 105, fam: "Сидоров", im: "Сидор", otch: "Сидорович", balance: 250 },
+  //     { id: 110, fam: "Петров", im: "Пётр", otch: "Петрович", balance: 180 },
+  //     { id: 120, fam: "Григорьев", im: "Григорий", otch: "Григорьевич", balance: -220 },
+  //   ],
+  //   2
+  // )).toEqual([
+  //   { id: 101, fam: "Иванов", im: "Иван", otch: "Иванович", balance: 200 },
+  //   { id: 110, fam: "Петров", im: "Пётр", otch: "Петрович", balance: 180 },
+  //   { id: 120, fam: "Григорьев", im: "Григорий", otch: "Григорьевич", balance: -220 },
+  //   { id: 130, fam: "Савельев", im: "Савелий", otch: "Савельевич", balance: 800 },
+  // ]);
 
 });
--- FD3/dz-6-tests/Mobile/modules/toDelete.js
+++ FD3/dz-6-tests/Mobile/modules/toDelete.js
@@ -1,7 +1,6 @@
 "use strict";
 
 function toDelete(inf, clientsStartArray) {
-
     let newClients = [...clientsStartArray];
     newClients = newClients.filter(function (i) {
         return i !== inf
