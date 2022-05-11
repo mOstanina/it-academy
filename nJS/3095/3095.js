7c2c9a923689738e813cd2040f68488cc74f0806
mariya.ostanina <maryiaost@gmail.com>
2022-05-11 20:29:15 +0300

    3095/ add log

--- nJS/3095/3095.js
+++ nJS/3095/3095.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 const webserver = express();
 const bodyParser = require("body-parser");
 webserver.use(express.static(path.join(__dirname, "public")));
@@ -8,20 +9,30 @@ webserver.use(bodyParser.json());
 webserver.use(express.urlencoded({extended:true}));
 
 const port = 3095;
+const logFN = path.join(__dirname, '_server.log');
+
+function logLineSync(logFilePath,logLine) {
+    const logDT=new Date();
+    let time=logDT.toLocaleDateString()+" "+logDT.toLocaleTimeString();
+    let fullLogLine=time+" "+logLine;
+    const logFd = fs.openSync(logFilePath, 'a+');
+    fs.writeSync(logFd, fullLogLine + os.EOL);
+    fs.closeSync(logFd);
+}
 
 webserver.get('/variants', (req, res) => {
+    logLineSync(logFN,`[${port}] `+"variants called, get pars: "+JSON.stringify(req.query));
     res.send(fs.readFileSync('vote.json', 'utf8'));
 });
 
 webserver.post('/vote', (req, res) => {
+    logLineSync(logFN,`[${port}] `+"vote called, get pars: "+JSON.stringify(req.query));
     const json1 = fs.readFileSync('vote.json', 'utf8');
     const object = JSON.parse(json1);
-    console.log(req)
-    console.log(req.body)
     object.variants.forEach(function(item, i, arr) {
        if(item.id===req.body.id){
           item.count++
-       };
+       }
     });
     const json2 = JSON.stringify(object);
     fs.writeFileSync('vote.json', json2);
@@ -29,9 +40,10 @@ webserver.post('/vote', (req, res) => {
 });
 
 webserver.post('/stat', (req, res) => {
+    logLineSync(logFN,`[${port}] `+"stat called, get pars: "+JSON.stringify(req.query));
     res.send(fs.readFileSync('vote.json', 'utf8'));
 });
 
 webserver.listen(port,()=>{
-    console.log("web server running on port " + port);
+    logLineSync(logFN,"web server running on port "+port);
 });
